7d1b70cea02be4250e81fedff76b421ff7f7118e
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2022-10-12 23:30:50 +0530

    fix: name

--- src/components/TextCard/TextCard.jsx
+++ src/components/TextCard/TextCard.jsx
@@ -9,8 +9,8 @@ const TextCard = ({card, isActive, setUsage}) => {
     return(
         <div className="card text-card" style={{ border: isActive ? "2px solid #5A4AD1": "2px solid #e2ebf6" }} onClick={cardHandler}>
             <img src={card.id === 1 ? user: group} alt={card.title} className="card-img"/>
-            <span class="card-title">{card.title}</span>
-            <p class="card-content">{card.text}</p>
+            <span className="card-title">{card.title}</span>
+            <p className="card-content">{card.text}</p>
         </div>
     );   
 }
