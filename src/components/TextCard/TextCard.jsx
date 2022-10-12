3ec40e2516a4096de68dc36d7afac73c87e681b1
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2022-10-12 23:08:17 +0530

    feat: textcard component

---
+++ src/components/TextCard/TextCard.jsx
@@ -0,0 +1,18 @@
+import user from "../../assets/userimg.png";
+import group from "../../assets/groupimg.png";
+import "./textCard.css";
+
+const TextCard = ({card, isActive, setUsage}) => {
+    const cardHandler = () => {
+        setUsage({'usage':card.id})
+    }
+    return(
+        <div className="card text-card" style={{ border: isActive ? "2px solid #5A4AD1": "2px solid #e2ebf6" }} onClick={cardHandler}>
+            <img src={card.id === 1 ? user: group} alt={card.title} className="card-img"/>
+            <span class="card-title">{card.title}</span>
+            <p class="card-content">{card.text}</p>
+        </div>
+    );   
+}
+
+export {TextCard};
