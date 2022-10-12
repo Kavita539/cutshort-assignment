cefec865aa19eb47f878f2467b1158d16fc657be
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2022-10-12 23:07:39 +0530

    feat: button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import "./button.css";
+
+const Button = ({text, clickHandler}) => {
+    return(
+        <button type="submit" className="form-btn" onClick={clickHandler}>{text}</button>
+    );
+}
+
+export {Button};
